cc64fbc29ad64e91aaed9e334ca6ca1b9acaef2a
whoopseez <whoopseez@gmail.com>
2017-07-17 15:43:57 -0400

    centered upload workout

--- client/upload.js
+++ client/upload.js
@@ -1,19 +1,24 @@
 Template.upload.events({
-	"click #js-submit" (event, instance) {
-		alert("Workout successfully submitted.")
-		console.log("hey it worked");
-		var name = $("#js-name").val();
-		var description = $("#js-description").val();
-		var tag = $("#js-tag").val();
+  "click #js-submit" (event, instance) {
+    alert("Workout successfully submitted.")
+    console.log("hey it worked");
+    var name = $("#js-name").val();
+    var description = $("#js-description").val();
+    var tag = $("#js-tag").val();
 
-		p = {
-			name: name,
-			description: description,
-			tag: tag,
-			createdBy: new Date(),
-			user: Meteor.userId()
-		};
-		console.dir(p);
-		Workouts.insert(p);
-	},
+    p = {
+      name: name,
+      description: description,
+      tag: tag,
+      createdBy: new Date(),
+      user: Meteor.userId()
+    };
+    console.dir(p);
+    Workouts.insert(p);
+  },
+  "click a" (event, instance) {
+    Accounts._loginButtonsSession.set('dropdownVisible', true);
+    console.log('dropdownVisible');
+  }
+
 })
